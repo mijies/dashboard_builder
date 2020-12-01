a81f462db32c5ef822a07d912da37c63dc19db8d
seijimatsuda <seiji3030@gmail.com>
2020-12-01 17:39:35 +0900

    account info with gui input

--- account/dialogbox.go
+++ account/dialogbox.go
@@ -9,61 +9,37 @@ import (
 
 type mainWindow struct {
 	*walk.MainWindow
-	// acc   *UserAccount
-}
-
-type dialogWindow struct{
-	dlg      	*walk.Dialog
 	user_name   *walk.LineEdit
 	login_name	*walk.LineEdit
 	acceptPB 	*walk.PushButton
-	cancelPB 	*walk.PushButton
 }
 
-func(w *dialogWindow) acceptCliked(acc *UserAccount) { 
+func(w *mainWindow) acceptClick(acc *UserAccount) { 
 	acc.Name = w.user_name.Text()
 	acc.Login_name = w.login_name.Text()
-	w.dlg.Accept()
+	w.Close()
 }
 
 func get_account_from_dialogbox() *UserAccount {
-
 	acc := &UserAccount{}
 	mainW := &mainWindow{}
 
-	mw := MainWindow{
+	if _, err := (MainWindow{
 		AssignTo: &mainW.MainWindow,
 		Visible: true,
-		Title: "DialogBoxテスト",
-		MinSize: Size {150, 200},
-		Size   : Size {300, 300},
+		Title: "Account Info",
+		MinSize: Size {300, 100},
+		// Size   : Size {300, 300},
 		Layout: VBox {},
-	}
-	// func(MainWindow) {
-	// 	return
-	// }(mw)
-	if _, err := mw.Run(); err != nil {
-		log.Fatal(err)
-		// os.Exit(1)
-	}
-
-	dlgW := new(dialogWindow)
-	dlg := Dialog{
-		AssignTo:      &dlgW.dlg,
-		Title:         "User Information",
-		DefaultButton: &dlgW.acceptPB,
-		CancelButton:  &dlgW.cancelPB,
-		MinSize: Size{300, 100},
-		Layout:  VBox{},
 		Children: []Widget{
 			Composite{
 				Layout: HBox{},
 				Children: []Widget{
 					Label{
-						Text: "User Name: ",
+						Text: "User Name :",
 					},
 					LineEdit{
-						AssignTo: &dlgW.user_name,
+						AssignTo: &mainW.user_name,
 					},
 				},
 			},
@@ -74,7 +50,7 @@ func get_account_from_dialogbox() *UserAccount {
 						Text: "Login Name:",
 					},
 					LineEdit{
-						AssignTo: &dlgW.login_name,
+						AssignTo: &mainW.login_name,
 					},
 				},
 			},
@@ -82,29 +58,17 @@ func get_account_from_dialogbox() *UserAccount {
 				Layout: HBox{},
 				Children: []Widget{
 					PushButton{
-						AssignTo: &dlgW.acceptPB,
+						AssignTo: &mainW.acceptPB,
 						Text:     "OK",
 						OnClicked: func(){
-							dlgW.acceptCliked(acc)
+							mainW.acceptClick(acc)
 						},
-					},
-					PushButton{
-						AssignTo:  &dlgW.cancelPB,
-						Text:      "Cancel",
-						OnClicked: func() { dlgW.dlg.Cancel() },
 					},
 				},
 			},
 		},
-	}
-
-	cmd, err := dlg.Run(walk.Form(mainW))
-	if err != nil {
+	}).Run(); err != nil {
 		log.Fatal(err)
-	} else if cmd == walk.DlgCmdCancel { 
-		return acc
-	} else if cmd == walk.DlgCmdNone {
-		return acc
 	}
 	return acc
 }
